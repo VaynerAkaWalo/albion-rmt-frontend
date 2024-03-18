a3da639ba2f3cc3fc62079c06d3776358967aa74
VaynerAkaWalo <vaynerakawalo@gmail.com>
2024-03-18 21:56:20 +0100

    header items as list

--- src/components/headers/Header.jsx
+++ src/components/headers/Header.jsx
@@ -2,11 +2,17 @@ import {Link} from "react-router-dom";
 
 function Header() {
     return (
-        <div className="w-full p-2 border-b-2 flex justify-evenly">
-            <Link to={"/calculator"}>Calculator</Link>
-            <Link to={"/"}>Albion RMT Empire Tool</Link>
-            <Link to={"/flips"}>BM Flips</Link>
-        </div>
+        <ul className="w-full p-2 border-b-2 flex justify-evenly">
+            <li>
+                <Link to={"/calculator"}>Calculator</Link>
+            </li>
+            <li>
+                <Link to={"/"}>Albion RMT Empire Tool</Link>
+            </li>
+            <li>
+                <Link to={"/flips"}>BM Flips</Link>
+            </li>
+        </ul>
     )
 }
 
