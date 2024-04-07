1b3b2d754b9925e9d7ac1ce77f3affa4fca57bc6
VaynerAkaWalo <58806342+VaynerAkaWalo@users.noreply.github.com>
2024-04-08 01:21:26 +0200

    fix select and some item positioning fixes (#25)

--- src/components/calculator/LocationSelector.jsx
+++ src/components/calculator/LocationSelector.jsx
@@ -1,27 +1,34 @@
 import {SelectComponent} from "../utils/SelectComponent.jsx";
+import Select from "react-select";
 
 export function LocationSelector() {
+    const cities = ["Thetford", "Fort Sterling", "Caerleon", "Personal Island", "Hideout", "City w/o return"]
+    function createOptions() {
+        const array = [];
+
+        cities.map(city => array.push({label: city, value: city}))
+
+        return array;
+    }
+
     return (
-        <div className="border-b columns-2 p-4 [&>*]:mx-2">
-            <SelectComponent {...locations}/>
+        <div className="border-b grid grid-cols-2 [&>*]:m-auto">
+            <Select className="w-4/5" menuPosition={"fixed"} options={createOptions()} styles={customStyles} isSearchable={false} />
             <div className="flex flex-col text-center">
-                <label htmlFor="preffered city">Recommended City</label>
-                <span id="preffered city">Thetford</span>
+                <span>Recommended City</span>
+                <span>Thetford</span>
             </div>
         </div>
     )
 }
 
-var locations = {
-    name: "Selected location",
-    label: true,
-    items: [
-        "Thetford",
-        "Fort Sterling",
-        "Caerleon",
-        "Personal Island",
-        "Hideout",
-        "City w/o return",
-        "Custom"
-    ]
-}
+const customStyles = {
+    control: (provided) => ({
+        ...provided,
+        backgroundColor: "none"// your font declaration
+    }),
+    singleValue: (provided) => ({
+        ...provided,
+        color: "white"
+    })
+};
