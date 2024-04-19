3fec45848cea927e35f1d8013f56d5687f939b7c
VaynerAkaWalo <vaynerakawalo@gmail.com>
2024-04-20 01:02:48 +0200

    display last seen time

--- src/components/blackmarket/BMItemRow.jsx
+++ src/components/blackmarket/BMItemRow.jsx
@@ -1,6 +1,11 @@
 import {intFormatter} from "../calculator/scripts/utils.js";
 
 export function BMItemRow({systemName, amount, orderId, unitPrice, tier, enchant, quality, lastUpdate}) {
+    let duration = Date.now() - lastUpdate
+
+    let minutes = Math.round(duration / 1000 / 60) % 60
+    let hours = Math.round(duration / 1000 / 3600) % 24
+    let days = Math.round(duration / 1000 / 3600 / 24)
 
     function image() {
         return `https://render.albiononline.com/v1/item/T${tier}_${systemName.toUpperCase()}${enchantPostfix()}.png?quality=${quality}`
@@ -11,26 +16,7 @@ export function BMItemRow({systemName, amount, orderId, unitPrice, tier, enchant
         return `@${enchant}`
     }
 
-    function convertToHHMMSS( then ) {
-        let duration = Date.now() - then
-
-        let minutes = Math.round(duration / 1000 / 60) % 60
-        let hours = Math.round(duration / 1000 / 3600) % 24
-        let days = Math.round(duration / 1000 / 3600 / 24)
-
-        hours = (hours < 10) ? "0" + hours : hours;
-        minutes = (minutes < 10) ? "0" + minutes : minutes;
-
-        return days + ":" + hours + ":" + minutes;
-    }
-
     function prettyTime() {
-        let duration = Date.now() - lastUpdate
-
-        let minutes = Math.round(duration / 1000 / 60) % 60
-        let hours = Math.round(duration / 1000 / 3600) % 24
-        let days = Math.round(duration / 1000 / 3600 / 24)
-
         if (days > 0) {
             return `${days}d ${hours}h`
         }
@@ -48,13 +34,25 @@ export function BMItemRow({systemName, amount, orderId, unitPrice, tier, enchant
         })
     }
 
+    function resolveColor() {
+        if (hours > 3) {
+            return "text-red-600"
+        }
+        else if(hours > 0 || minutes > 30) {
+            return "text-yellow-400"
+        }
+        else {
+            return "text-green-600"
+        }
+    }
+
     return (
         <div className="w-full px-10 py-2 h-30 flex justify-between border-2 [&>*]: items-center">
             <img src={image()} alt="xd"/>
             <div>amount: {intFormatter(amount)}</div>
-            <div>last seen: {prettyTime()}</div>
+            <div className={resolveColor()}>last seen: {prettyTime()}</div>
             <div>price: {intFormatter(unitPrice / 10000)}</div>
-            <button className="font-bold border-2 rounded p-6 bg-red-700 border-red-700" onClick={deleteItem}>Delete Order</button>
+            <button className="font-bold border-2 rounded p-6 bg-red-700 border-red-700 text-white" onClick={deleteItem}>Delete Order</button>
         </div>
     )
 }
