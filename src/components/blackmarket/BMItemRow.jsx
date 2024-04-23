7cdac4a67d729caa878c2df391b823d5acef54e3
VaynerAkaWalo <vaynerakawalo@gmail.com>
2024-04-23 23:14:58 +0200

    Fix mb transmutation cost

--- src/components/blackmarket/BMItemRow.jsx
+++ src/components/blackmarket/BMItemRow.jsx
@@ -59,7 +59,7 @@ export function BMItemRow({systemName, amount, orderId, unitPrice, tier, enchant
         if (tier < 4) {
             return "Unavailable"
         }
-        return intFormatter(transmutationCost[tier - 4][enchant] * (getResourceAmount(resourceOne) + getResourceAmount(resourceTwo))) / 2
+        return intFormatter(transmutationCost[tier - 4][enchant] * (getResourceAmount(resourceOne) + getResourceAmount(resourceTwo)) / 2)
 
     }
 
