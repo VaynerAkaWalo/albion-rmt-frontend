258bae21c2eefb82b9bc8221e486413564bea435
VaynerAkaWalo <vaynerakawalo@gmail.com>
2024-04-24 01:42:00 +0200

    Set prices to transmutation cost

--- src/components/calculator/ResourcesPanel.jsx
+++ src/components/calculator/ResourcesPanel.jsx
@@ -1,12 +1,22 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 import {GlobalContext} from "./context/CalculatorContext.jsx";
 import trash from "/T1_TRASH.png";
 import {intFormatter} from "./scripts/utils.js";
+import {transmutationCost} from "../../utils/TransmutationUtil.js";
+
+const TIER_OFFSET = 4
 
 export function ResourcesPanel() {
     const {selectedItem, isInitialized, detailedItemInfo, setDetailedItemInfo, settings} = useContext(GlobalContext)
     const {resourceOnePrice, resourceTwoPrice, resourceOneAmount, resourceTwoAmount, resourceTwo} = detailedItemInfo
     const {amountMultiplier} = settings
+    const {tier, enchant} = selectedItem
+
+    useEffect(() => {
+        if (!tier) return
+        setDetailedItemInfo(prev => ({...prev, resourceOnePrice: transmutationCost[tier - 4][enchant] / 2}))
+        setDetailedItemInfo(prev => ({...prev, resourceTwoPrice: transmutationCost[tier - 4][enchant] / 2}))
+    }, [selectedItem]);
 
     function image(resource) {
         if (!isInitialized) return trash;
